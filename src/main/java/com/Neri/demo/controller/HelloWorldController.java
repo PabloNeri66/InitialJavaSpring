de62aa9f86b79de5e23625f306bdeefce54bd25f
Pablo Alves Neri <pabloneri66@gmail.com>
2025-03-15 12:13:36 -0300

    Testando

--- src/main/java/com/Neri/demo/controller/HelloWorldController.java
+++ src/main/java/com/Neri/demo/controller/HelloWorldController.java
@@ -1,8 +1,18 @@
 package com.Neri.demo.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello-world")
 
+
+
+// STATEFUL --> O estado do cliente é mantido : STATELESS --> (Token) A cada nova requisicao preciso enviar dados e informacoes ( mais escalavel )
 public class HelloWorldController {
-
+    @GetMapping("/get")
+    public String helloworld()
+    {
+        return "hello World";
+    }
 }
